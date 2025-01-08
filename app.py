c9b5e99b6b973e3bff462355de3ab63c14e91ff3
hank.aungkyaw <hank.lo.kyaw@gmail.com>
2025-01-07 21:34:58 -0800

    added backend py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ df = pd.read_csv('SKU_Description.csv')
 
 print(df)
 
+# Choose "exact" or "wildcard" for search option
+SEARCH_METHOD = "wildcard"
+
 def search_sku_description(search_term, column, match_type="exact"):
     """
     Search the DataFrame based on the specified column and match type.
@@ -27,31 +30,34 @@ def search_sku_description(search_term, column, match_type="exact"):
         raise ValueError("Invalid match type. Use 'exact' or 'wildcard'.")
     return filtered_df
 
+search_df = search_sku_description("tesla", "Description", match_type=SEARCH_METHOD)
+print(search_df)
 
-# app = Flask(__name__)
-#
-# @app.route('/search', methods=['GET'])
-# def search():
-#     term = request.args.get('term', '')
-#     column = request.args.get('column', 'SKU')
-#     match_type = request.args.get('match_type', 'exact')
-#
-#     if column not in ['SKU', 'Description']:
-#         return jsonify({'error': "Invalid column. Use 'SKU' or 'Description'."}), 400
-#
-#     try:
-#         result = search_sku_description(term, column, match_type)
-#     except ValueError as e:
-#         return jsonify({'error': str(e)}), 400
-#
-#     if result.empty:
-#         return jsonify({'error': 'No matches found'}), 404
-#
-#     return jsonify(result.to_dict(orient='records'))
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=True, port=5003)
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['GET'])
+def search():
+    term = request.args.get('term', '')
+    column = request.args.get('column', 'SKU')
+    match_type = request.args.get('match_type', 'exact')
+
+    if column not in ['SKU', 'Description']:
+        return jsonify({'error': "Invalid column. Use 'SKU' or 'Description'."}), 400
+
+    try:
+        result = search_sku_description(term, column, match_type)
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+    if result.empty:
+        return jsonify({'error': 'No matches found'}), 404
+
+    return jsonify(result.to_dict(orient='records'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5003)
